366f869b4af920ecfcdfae5c35c7193678db7022
WelfarenNN <26lp89151895@G401-CC0213.local>
2026-09-15 09:50:52 +0800

    updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get("/api/health", (request, response) => {
 });
 
 app.use("/auth", AuthRouter);
-app.use("/food-category", FoodCategoryRouter);
+app.use("/food-category", FoodCategoryRouter);  
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
